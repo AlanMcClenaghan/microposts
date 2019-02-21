5532bfe11e468c3881e051222788a634e8d6226d
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-02-21 21:19:45 +0000

    Add posts and show alert.

--- src/http.js
+++ src/http.js
@@ -1,16 +1,15 @@
-/*********
- *
+/**
  * EasyHTTP Library
  * Library for making HTTP requests
  *
  * @version 3.0.0
- * @author  Alan McClenaghan
+ * @author  Brad Traversy
  * @license MIT
  *
- *********/
+ **/
 
 class EasyHTTP {
-  // Make an HTTP GET Request
+  // Make an HTTP GET Request 
   async get(url) {
     const response = await fetch(url);
     const resData = await response.json();
@@ -26,12 +25,14 @@ class EasyHTTP {
       },
       body: JSON.stringify(data)
     });
+
     const resData = await response.json();
     return resData;
+   
   }
 
-  // Make an HTTP PUT Request
-  async put(url, data) {
+   // Make an HTTP PUT Request
+   async put(url, data) {
     const response = await fetch(url, {
       method: 'PUT',
       headers: {
@@ -39,21 +40,26 @@ class EasyHTTP {
       },
       body: JSON.stringify(data)
     });
+    
     const resData = await response.json();
     return resData;
   }
 
-  // Make an HTTP PUT Request
-  async delete(url, data) {
+  // Make an HTTP DELETE Request
+  async delete(url) {
     const response = await fetch(url, {
       method: 'DELETE',
       headers: {
         'Content-type': 'application/json'
-      },
+      }
     });
+
     const resData = await 'Resource Deleted...';
     return resData;
   }
-}
 
-export const http = new EasyHTTP();
+ }
+
+ export const http = new EasyHTTP();
+
+ 
