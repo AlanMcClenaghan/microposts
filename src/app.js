1e1e219de6fd98366b4c4f2c01b6dd48e8b2d505
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-02-22 15:04:21 +0000

    Validation and Edit Posts.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost);
 // Listen for delete post
 document.querySelector('#posts').addEventListener('click', deletePost);
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for edit state
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 // Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -19,22 +25,42 @@ function getPosts() {
 
 // Submit Post
 function submitPost() {
+
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title,
     body
   }
 
-  // Create Post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post Added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  // Validate input
+  if (title === '' || body === '') {
+    ui.showAlert('Please add a title and body for your post', 'alert alert-danger')
+  } else {
+    // Check for ID
+    if (id === '') {
+      // Create Post
+      http.post('http://localhost:3000/posts', data)
+        .then(data => {
+          ui.showAlert('Post Added', 'alert alert-success');
+          ui.clearFields();
+          getPosts();
+        })
+        .catch(err => console.log(err));
+    } else {
+      // Update Post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+        .then(data => {
+          ui.showAlert('Post Updated', 'alert alert-success');
+          ui.changeFormState('add');
+          getPosts();
+        })
+        .catch(err => console.log(err));
+
+    }
+  }
 }
 
 // Delete Post
@@ -52,4 +78,32 @@ function deletePost(e) {
         .catch(err => console.log(err));
     }
   }
+}
+
+// Enable Edit State
+function enableEdit(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill form with current post
+    ui.fillForm(data);
+  }
+}
+
+// Cancel Edit State
+function cancelEdit(e) {
+  if (e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
+  e.preventDefault();
 }
